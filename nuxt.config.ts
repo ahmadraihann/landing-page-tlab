65ce443b49d5a30aedb24cd2727e6985d470d9f0
ahmadraihann <ahmadraihan015@gmail.com>
2023-04-07 21:03:07 +0700

    UPDATE: Update title meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  app: {
+    head: {
+      charset: "utf-16",
+      viewport: "width=500, initial-scale=1",
+      title: "TLab",
+      meta: [{ name: "description", content: "TLab" }],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/icon.ico" }],
+    },
+  },
+});
